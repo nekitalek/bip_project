7d5e7fe371cf99e32f3c568040cea31a65d3b66d
Nikita Dedov <nikita.tars@inbox.ru>
2023-04-06 23:55:17 +0300

    reposition, scaling and absoluting

--- src/map.js
+++ src/map.js
@@ -1,15 +1,26 @@
     // Функция ymaps.ready() будет вызвана, когда
     // загрузятся все компоненты API, а также когда будет готово DOM-дерево.
-    ymaps.ready(init);
-    function init(){
-        // Создание карты.
-        var myMap = new ymaps.Map("map", {
+    function init() {
+        var myMap = new ymaps.Map('map', {
             center: [59.939098, 30.315868],
             zoom: 11,
-            type: 'yandex#map',
-            controls: ['zoomControl', 'geolocationControl']
-        }, {
-            searchControlProvider: 'yandex#search'
+            controls: []
         });
-    }
+        
+        // Создадим экземпляр элемента управления «поиск по карте»
+        // с установленной опцией провайдера данных для поиска по организациям.
+        var searchControl = new ymaps.control.SearchControl({
+            options: {
+                provider: 'yandex#search'
+            }
+        });
+        
+        // myMap.controls.add(searchControl);
+        
+        // Программно выполним поиск определённых кафе в текущей
+        // прямоугольной области карты.
+        //searchControl.search('Шоколадница');
+    } 
+    ymaps.ready(init);
 
+      
