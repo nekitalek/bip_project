ab3da19f97535f2f3909ad8521d1b2e783ed6e91
Nikita Dedov <nikita.tars@inbox.ru>
2023-09-07 21:17:13 +0300

    not added function

--- frontend/src/main_requests.js
+++ frontend/src/main_requests.js
@@ -1,11 +1,17 @@
 const requestURL = 'https://jsonplaceholder.typicode.com/users'
 
-function sendRequest(method, url, body = null){
-    return fetch(url).then(response =>{
+async function sendRequest(method, url, body = null){
+    return await fetch(url).then(response =>{
         return response.json()
     })
 }
 
+let email = document.querySelector('#email_setting')
+
 sendRequest('GET', requestURL)
     .then(data => console.log(data))
     .catch(err => console.log(err))
+
+    let key = 1
+    email.innerHTML += '<input id="email_setting" name="email" type="email" autocomplete="email" value="${data[key].email}" class="block w-full rounded-md border-0 py-1.5 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">'
+
