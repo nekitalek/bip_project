b183c37c7d9cd97cac38facbf3eb0f015ebd2330
Nikita Dedov <nikita.tars@inbox.ru>
2023-09-16 16:38:51 +0300

    added settings request

--- frontend/src/main_requests.js
+++ frontend/src/main_requests.js
@@ -6,11 +6,14 @@ async function sendRequest(method, url, body = null){
     })
 }
 
-let email = document.querySelector('.email_recieved')
+
+
 let k = 1
 
 sendRequest('GET', requestURL)
-    .then(data => console.log(data[k].email))
+    .then(function(data){
+        let username = document.getElementById("username").value = data[k].username;
+        let email = document.getElementById("email_setting").value = data[k].email;
+        let phone = document.getElementById("phone").value = data[k].phone;
+    })
     .catch(err => console.log(err))
-    email.innerHTML += '<input id="email_setting" name="email" type="email" autocomplete="email" value="${data[k].email}" class="block w-full rounded-md border-0 py-1.5 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">'
-
