cf7d9dc1b0f1b579c2629dc5e7f05fb3ac3752ec
Di-id <Dimasfedorov@rambler.ru>
2023-09-29 22:42:22 +0300

    ServSettings

--- backend/internal/transport/handler/handler.go
+++ backend/internal/transport/handler/handler.go
@@ -12,6 +12,23 @@ type Handler struct {
 	services *service.Service
 }
 
+func CORSMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+
+		c.Header("Access-Control-Allow-Origin", "*")
+		c.Header("Access-Control-Allow-Credentials", "true")
+		c.Header("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
+		c.Header("Access-Control-Allow-Methods", "POST,HEAD,PATCH, OPTIONS, GET, PUT")
+
+		if c.Request.Method == "OPTIONS" {
+			c.AbortWithStatus(204)
+			return
+		}
+
+		c.Next()
+	}
+}
+
 func NewHandler(services *service.Service) *Handler {
 	return &Handler{services: services}
 }
@@ -19,7 +36,7 @@ func NewHandler(services *service.Service) *Handler {
 func (h *Handler) InitRoutes() *gin.Engine {
 	router := gin.New()
 	router.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
-
+	router.Use(CORSMiddleware())
 	router.Use(h.CSRFIdentity())
 	router.GET("/CSRF", h.GetCSRF)
 
