724ad3db975ebec868531b0bb876a7b0791c06a4
nekitalek <nekitness@gmail.com>
2023-10-06 02:32:05 +0300

    3463463456

--- frontend/src/history/js/history.js
+++ frontend/src/history/js/history.js
@@ -99,7 +99,7 @@ function LeftEvent(button_id){
   var event_id = parseInt(button_id)
   const token = localStorage.getItem('token_CSRF');
   const auth_token = localStorage.getItem('auth_token');
-  const user_id = localStorage.getItem('user_id');
+  const user_id = parseInt(localStorage.getItem('user_id'));
   const xhr = new XMLHttpRequest();
   xhr.open("DELETE", "https://51.250.24.31:65000/api/invitation/" + event_id,false);
   xhr.setRequestHeader("Content-Type", "application/json; charset=UTF-8");
