5d9b1bd3720735eae9f33348896cb6e9a1b7b58a
nekitalek <nekitness@gmail.com>
2023-10-06 01:16:55 +0300

    added join + left event

--- frontend/src/history/js/history.js
+++ frontend/src/history/js/history.js
@@ -26,20 +26,6 @@ function GetEvents(){
     } catch(err) {
     alert("Запрос не удался");
     }
-
-    /*const flattenJSON = (event_list, res = {}, extraKey = '') => {
-      for(key in event_list){
-         if(typeof event_list[key] !== 'object'){
-            res[extraKey + key] = event_list[key];
-         }else{
-            flattenJSON(event_list[key], res, `${extraKey}${key}.`);
-         };
-      };
-      return res;
-   };
-   var flat_event_list = flattenJSON(event_list)
-   console.log(flat_event_list)*/
-
     var out = '';
     for (var key in event_list){
       var participants = event_list[key].participant;
@@ -49,7 +35,82 @@ function GetEvents(){
         console.log(all_part)
       }
 
-      out += '<a href="#" class="flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">  <div class="flex flex-col justify-between p-4 leading-normal"><h4 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Описание мероприятия ' + event_list[key].description +'</h4><p id="time_start" class="mb-3 font-normal text-gray-700 dark:text-gray-400">Время начала: '+ event_list[key].time_start +'</p><p id="time_end" class="mb-3 font-normal text-gray-700 dark:text-gray-400">Время окончания: '+ event_list[key].time_end +'</p><p id="place" class="mb-3 font-normal text-gray-700 dark:text-gray-400">Адрес: '+ event_list[key].place +'</p><p id="game" class="mb-3 font-normal text-gray-700 dark:text-gray-400">Игра: '+ event_list[key].game +'</p><p id="participant" class="mb-3 font-normal text-gray-700 dark:text-gray-400">Участники: '+ all_part +'</p><button id="reg_event_'+event_list[key].event_items_id+'" type="button" class="focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">Записаться</button><button id="decline_event" type="button" class="focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900">Хуй знает че сделать</button></div></a>';
+      out += '<a href="#" class="flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">  <div class="flex flex-col justify-between p-4 leading-normal"><h4 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Описание мероприятия ' + event_list[key].description +'</h4><p id="time_start" class="mb-3 font-normal text-gray-700 dark:text-gray-400">Время начала: '+ event_list[key].time_start +'</p><p id="time_end" class="mb-3 font-normal text-gray-700 dark:text-gray-400">Время окончания: '+ event_list[key].time_end +'</p><p id="place" class="mb-3 font-normal text-gray-700 dark:text-gray-400">Адрес: '+ event_list[key].place +'</p><p id="game" class="mb-3 font-normal text-gray-700 dark:text-gray-400">Игра: '+ event_list[key].game +'</p><p id="participant" class="mb-3 font-normal text-gray-700 dark:text-gray-400">Участники: '+ all_part +'</p><button id="'+event_list[key].event_items_id+'_accept_event" type="button" class="focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">Записаться</button><button id="'+event_list[key].event_items_id+'_decline_event" type="button" class="focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900">Покинуть мероприятие</button></div></a>';
     }
     document.getElementById('event_grid').innerHTML = out;
+}
+
+
+window.addEventListener("DOMContentLoaded", (event) => {
+  var buttons = document.querySelectorAll('button');
+
+  for (var i=0; i<buttons.length; ++i) {
+    buttons[i].addEventListener('click', clickFunc);
+  }
+
+  const comp_string = 'add'
+  function clickFunc() {
+    if(comp_string < comp_string){
+      JoinEvent(this.id); 
+    }
+    else{
+      LeftEvent(this.id)
+    }
+  }
+});
+
+
+function JoinEvent(button_id){
+    var event_id = parseInt(button_id)
+    const token = localStorage.getItem('token_CSRF');
+    const auth_token = localStorage.getItem('auth_token');
+    const user_id = localStorage.getItem('user_id');
+    const xhr = new XMLHttpRequest();
+    xhr.open("POST", "https://51.250.24.31:65000/api/invitation/",false);
+    xhr.setRequestHeader("Content-Type", "application/json; charset=UTF-8");
+    xhr.setRequestHeader("X-CSRF-TOKEN", token);
+    xhr.setRequestHeader("Authorization", "Bearer " + auth_token);
+    xhr.withCredentials = true;
+
+    const body = JSON.stringify({
+        "event_id": event_id,
+        "user_id": user_id,
+        "status": "Confirmed"
+    });
+    xhr.onload = () => {
+      if (xhr.readyState == 4 && xhr.status == 201) {
+        console.log(JSON.parse(xhr.responseText));
+      } else {
+        console.log(`Error: ${xhr.status}`);
+      }
+    };
+    xhr.send(body);
+    console.log(body)
+}
+
+function LeftEvent(button_id){
+  var event_id = parseInt(button_id)
+  const token = localStorage.getItem('token_CSRF');
+  const auth_token = localStorage.getItem('auth_token');
+  const user_id = localStorage.getItem('user_id');
+  const xhr = new XMLHttpRequest();
+  xhr.open("DELETE", "https://51.250.24.31:65000/api/invitation/1/",false);
+  xhr.setRequestHeader("Content-Type", "application/json; charset=UTF-8");
+  xhr.setRequestHeader("X-CSRF-TOKEN", token);
+  xhr.setRequestHeader("Authorization", "Bearer " + auth_token);
+  xhr.withCredentials = true;
+
+  const body = JSON.stringify({
+      "event_id": event_id,
+      "user_id": user_id,
+  });
+  xhr.onload = () => {
+    if (xhr.readyState == 4 && xhr.status == 201) {
+      console.log(JSON.parse(xhr.responseText));
+    } else {
+      console.log(`Error: ${xhr.status}`);
+    }
+  };
+  xhr.send(body);
+  console.log(body)
 }
