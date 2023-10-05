bfe2211df31538cde495c0988f07faebabb54aac
nekitalek <nekitness@gmail.com>
2023-10-05 22:16:12 +0300

    modified:   frontend/src/main/js/main.js

--- frontend/src/main/js/main.js
+++ frontend/src/main/js/main.js
@@ -13,24 +13,24 @@ window.addEventListener("DOMContentLoaded", (event) => {
     xhr.setRequestHeader("Content-Type", "application/json; charset=UTF-8");
     xhr.setRequestHeader("X-CSRF-TOKEN", token);
 
-    //auth_token = localStorage.getItem('auth_token');
-    //xhr.setRequestHeader("Authorization", "Bearer " + auth_token);
+    auth_token = localStorage.getItem('auth_token');
+    xhr.setRequestHeader("Authorization", "Bearer " + auth_token);
     xhr.withCredentials = true;
   
     var time_start = document.getElementById("start_time").value;
     var time_end = document.getElementById("end_time").value;
     var place = document.getElementById("address").value;
     var description = document.getElementById("description").value;
-    //var public = document.getElementsById('checkbox').checked;
-    //const game = document.querySelector('input[name="list-radio"]:checked').value;
+    var public = document.getElementsById('checkbox').checked;
+    const game = document.querySelector('input[name="list-radio"]:checked').value;
 
     const body = JSON.stringify({
         "time_start": time_start,
         "time_end": time_end,
         "place": place,
-        "game": "game",
+        "game": game,
         "description": description,
-        "public": "public"
+        "public": public
     });
     xhr.onload = () => {
       if (xhr.readyState == 4 && xhr.status == 201) {
