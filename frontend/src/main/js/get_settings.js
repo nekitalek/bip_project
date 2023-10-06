13a25cd9ce0ffc84ce93fe3dffbc5bfd045b819f
Di-id <Dimasfedorov@rambler.ru>
2023-10-06 23:51:22 +0300

    .....

--- frontend/src/main/js/get_settings.js
+++ frontend/src/main/js/get_settings.js
@@ -1,5 +1,8 @@
 const xhr = new XMLHttpRequest();
-
+    const token = localStorage.getItem('token_CSRF')
+    const auth_token = localStorage.getItem('auth_token');
+    const user_id = localStorage.getItem('user_id')
+    
     xhr.open("GET", "https://51.250.24.31:65000/api/user/" + user_id ,false);
     xhr.setRequestHeader("Content-Type", "application/json; charset=UTF-8");
     xhr.setRequestHeader("X-CSRF-TOKEN", token);
--- frontend/src/settings/js/get_settings.js
+++ frontend/src/settings/js/get_settings.js
@@ -1,12 +1,16 @@
 const xhr_get = new XMLHttpRequest();
 
+    const token = localStorage.getItem('token_CSRF')
+    const auth_token = localStorage.getItem('auth_token');
+    const user_id = localStorage.getItem('user_id')
+
     xhr_get.open("GET", "https://51.250.24.31:65000/api/user/" + user_id ,false);
     xhr_get.setRequestHeader("Content-Type", "application/json; charset=UTF-8");
     xhr_get.setRequestHeader("X-CSRF-TOKEN", token);
     xhr_get.setRequestHeader("Authorization", "Bearer " + auth_token);
     xhr_get.withCredentials = true;
     xhr_get.send();
-    
+
     if(xhr.status!=200){
         window.location.href = "https://51.250.24.31/login/"; 
     }
