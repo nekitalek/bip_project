0f5c75a49b328cab7a7b140afc70a5cb094dea7b
Di-id <Dimasfedorov@rambler.ru>
2023-10-06 23:40:12 +0300

    correct

--- frontend/src/main/js/get_settings.js
+++ frontend/src/main/js/get_settings.js
@@ -1,16 +1,16 @@
-const xhr = new XMLHttpRequest();
+const xhr_get = new XMLHttpRequest();
     const token = localStorage.getItem('token_CSRF') // получения токена из локального хранилища
     const auth_token = localStorage.getItem('auth_token'); // получение auth_token из локального хранилища
 
     user_id = localStorage.getItem('user_id');
 
-    xhr.open("GET", "https://51.250.24.31:65000/api/user/" + user_id ,false);
-    xhr.setRequestHeader("Content-Type", "application/json; charset=UTF-8");
-    xhr.setRequestHeader("X-CSRF-TOKEN", token);
-    xhr.setRequestHeader("Authorization", "Bearer " + auth_token);
-    xhr.withCredentials = true;
-    xhr.send();
+    xhr_get.open("GET", "https://51.250.24.31:65000/api/user/" + user_id ,false);
+    xhr_get.setRequestHeader("Content-Type", "application/json; charset=UTF-8");
+    xhr_get.setRequestHeader("X-CSRF-TOKEN", token);
+    xhr_get.setRequestHeader("Authorization", "Bearer " + auth_token);
+    xhr_get.withCredentials = true;
+    xhr_get.send();
 
-    var jsonResponse = JSON.parse(xhr.responseText);
+    var jsonResponse = JSON.parse(xhr_get.responseText);
     document.getElementById("win_name").innerHTML = jsonResponse["username"];
     document.getElementById("win_email").innerHTML = jsonResponse["login"];
