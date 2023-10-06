f02215b1699cd607015b9ad58e805ba3d41d8856
Di-id <Dimasfedorov@rambler.ru>
2023-10-06 23:42:35 +0300

    correct2

--- frontend/src/settings/js/get_settings.js
+++ frontend/src/settings/js/get_settings.js
@@ -1,8 +1,21 @@
-const xhr_get = new XMLHttpRequest();
+const xhr = new XMLHttpRequest();
     const token = localStorage.getItem('token_CSRF') // получения токена из локального хранилища
     const auth_token = localStorage.getItem('auth_token'); // получение auth_token из локального хранилища
 
-    user_id = localStorage.getItem('user_id');
+    var user_id = localStorage.getItem('user_id');
+
+    xhr.open("GET", "https://51.250.24.31:65000/api/user/" + user_id ,false);
+    xhr.setRequestHeader("Content-Type", "application/json; charset=UTF-8");
+    xhr.setRequestHeader("X-CSRF-TOKEN", token);
+    xhr.setRequestHeader("Authorization", "Bearer " + auth_token);
+    xhr.withCredentials = true;
+    xhr.send();
+
+    if(xhr.status!=200){
+        window.location.href = "https://51.250.24.31/login/"; 
+    }
+
+const xhr_get = new XMLHttpRequest();
 
     xhr_get.open("GET", "https://51.250.24.31:65000/api/user/" + user_id ,false);
     xhr_get.setRequestHeader("Content-Type", "application/json; charset=UTF-8");
