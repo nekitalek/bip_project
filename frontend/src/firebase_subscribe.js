91e775b7d008327cd11a6f71c71ef3da67929fd3
nekitalek <nekitness@gmail.com>
2023-10-06 03:32:39 +0300

    5ty

--- frontend/src/firebase_subscribe.js
+++ frontend/src/firebase_subscribe.js
@@ -57,7 +57,7 @@ function sendTokenToServer(currentToken) {
 
         const xhr = new XMLHttpRequest();
         const token = localStorage.getItem('token_CSRF')
-        xhr.open("POST", "https://51.250.24.31:65000/api/push_notification",false);
+        xhr.open("POST", "https://51.250.24.31:65000/api/push_notification/",false);
         xhr.setRequestHeader("Content-Type", "application/json; charset=UTF-8");
         xhr.setRequestHeader("X-CSRF-TOKEN", token);
         xhr.withCredentials = true;
