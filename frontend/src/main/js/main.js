8e5032bebdc7c5a8543e8bebf2bb0435379364ab
nekialek <nekitness@gmail.com>
2023-10-06 16:37:40 +0300

    recode main.js

--- frontend/src/main/js/main.js
+++ frontend/src/main/js/main.js
@@ -1,22 +1,22 @@
-window.addEventListener("DOMContentLoaded", (event) => {
-    const el = document.getElementById('send_form');
-    if (el) {
-      el.addEventListener('click', CreateEvent);
-    }
-  });
+window.addEventListener("DOMContentLoaded", (event) => {  // ожидание полной загрузки страницы и добавление листенера на кнопку
+  document.getElementById('send_form').addEventListener('click', CreateEvent); // вызов функции проверки пароля по нажатию
+});
   
   function CreateEvent(){
   
+    //получаем данные из локального хранилища
     const token = localStorage.getItem('token_CSRF')
+    const auth_token = localStorage.getItem('auth_token');
+
+    //создаем новый запрос на создание события
     const xhr = new XMLHttpRequest();
     xhr.open("POST", "https://51.250.24.31:65000/api/event/",false);
     xhr.setRequestHeader("Content-Type", "application/json; charset=UTF-8");
     xhr.setRequestHeader("X-CSRF-TOKEN", token);
-
-    auth_token = localStorage.getItem('auth_token');
     xhr.setRequestHeader("Authorization", "Bearer " + auth_token);
     xhr.withCredentials = true;
   
+    // получаем переменные из html и засовываем в json
     var time_start = document.getElementById("start_time").value + ":00Z";
     var time_end = document.getElementById("end_time").value + ":00Z";
     var place = document.getElementById("address").value;
@@ -32,12 +32,7 @@ window.addEventListener("DOMContentLoaded", (event) => {
         "description": description,
         "public": public
     });
-    xhr.onload = () => {
-      if (xhr.readyState == 4 && xhr.status == 201) {
-        console.log(JSON.parse(xhr.responseText));
-      } else {
-        console.log(`Error: ${xhr.status}`);
-      }
-    };
+    
+    // отправляем запрос
     xhr.send(body);
   }
