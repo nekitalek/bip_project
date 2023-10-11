684d75556db86e63d53ec364cf51ef04ecda3bae
nekialek <nekitness@gmail.com>
2023-10-11 20:04:27 +0300

    started change settings

--- frontend/src/settings/js/change_settings.js
+++ frontend/src/settings/js/change_settings.js
@@ -1,13 +1,16 @@
-window.addEventListener("DOMContentLoaded", (event) => {
-    
-    
-    
-    
-    
+  window.addEventListener("DOMContentLoaded", (event) => {
     // добавить if на проверку каких либо изменений данных пользователем, иначе не делать ничего по нажатию кнопки
-    const el = document.getElementById('send_settings');
-    if (el) {
-      el.addEventListener('click', openModal);
+    const email = document.getElementById('send_email');
+    if (email) {
+      email.addEventListener('click', ChangeMail);
+    }
+  });
+
+  window.addEventListener("DOMContentLoaded", (event) => {
+    // добавить if на проверку каких либо изменений данных пользователем, иначе не делать ничего по нажатию кнопки
+    const password = document.getElementById('send_password');
+    if (password) {
+      password.addEventListener('click', ChangePassword);
     }
   });
   
@@ -20,65 +23,39 @@ window.addEventListener("DOMContentLoaded", (event) => {
     document.getElementById("2fa_modal").classList.add("hidden");
     }
   
-  function verify2fa(){
-  const requestURL = 'https://51.250.24.31/CSRF'
-  var xhr = new XMLHttpRequest()
-  xhr.open('GET', requestURL, false);
+  // функция проверки код второго фактора  
+function SecFactorEmail(){
+  var fa_code = document.getElementById("factor").value; // получение цисла из HTML
+  const token = localStorage.getItem('token_CSRF') // получение CSRF токена
+  const user_id = localStorage.getItem('user_id'); // получение user_id
+
+  // формируем запрос на 2фа
+  const xhr = new XMLHttpRequest();
+  xhr.open("POST", "https://51.250.24.31/auth/change/email/sec_factor",false);
+  xhr.setRequestHeader("Content-Type", "application/json; charset=UTF-8");
+  xhr.setRequestHeader("X-CSRF-TOKEN", token);
   xhr.withCredentials = true;
-  try {
-  xhr.send();
-  if (xhr.status != 200) {
-    console.log('Ошибка');
-  } else {
-    var jsonResponse = JSON.parse(xhr.responseText);
-    console.log(jsonResponse["token_CSRF"]);
-    localStorage.setItem('token_CSRF',jsonResponse["token_CSRF"]);
-    //если нужно использовать в другом скрипке, то сохранить в память браузера
-    //localStorage.setItem(jsonResponse["token_CSRF"]);
-    //Для получение .getItem
+
+  //получаем переменные из html и засовываем в json
+  const body = JSON.stringify({
+      "user_id": parseInt(user_id), 
+      "code": parseInt(fa_code),
+      "device": "windows"
+    });
+  
+  xhr.send(body); // отправляем запрос
+
+  // парсим ответ, если ок то перенаправляем на логин
+  if (xhr.status == 200){
+    var jsonResponse = JSON.parse(xhr.responseText); // парсим токен полученный в ответ от сервера
+    localStorage.setItem('auth_token',jsonResponse["auth_token"]); // кладем токен в локальное хранилище
+    window.location.href = "https://51.250.24.31/main/"; 
   }
-  } catch(err) {
-  alert("Запрос не удался");
+  else{
+    alert(jsonResponse)
   }
-        var fa_code = document.getElementById("factor").value;
-        if(fa_code){
-            fa2POST(fa_code)
-            //if (fa2POST){ // не знаю что возвращает 2фа но выполнение входа в случае успеха
-            ChangeSettings()
-            //}
-        }
-        else alert("please enter the number")
-  
-  
-  }
-  
-  const btnSend2FA = document.querySelector('btn-send-2fa')
-  
-  function fa2POST(fa_code){
-    const xhr = new XMLHttpRequest();
-    const token = localStorage.getItem('token_CSRF')
-  
-    xhr.open("POST", "https://51.250.24.31/auth/sign-up/sec_factor",false);
-    xhr.setRequestHeader("Content-Type", "application/json; charset=UTF-8");
-    xhr.setRequestHeader("X-CSRF-TOKEN", token);
-  
-    xhr.withCredentials = true;
-  
-    const body = JSON.stringify({
-        "user_id": 1, // я понятия не имею как брать юзер айди поэтому он захардкожен
-        "code": parseInt(fa_code),
-        "device": "windows"
-      });
-    xhr.onload = () => {
-        if (xhr.readyState == 4 && xhr.status == 201) {
-            console.log(JSON.parse(xhr.responseText));
-        } else {
-            console.log(`Error: ${xhr.status}`);
-        }
-    };
-    xhr.send(body);
-  }
-  
+}
+
   function ChangeSettings(){
   
     const token = localStorage.getItem('token_CSRF')
