addce61e859aade608caa8b3015351c5dbe88b89
nekialek <nekitness@gmail.com>
2023-10-12 13:47:00 +0300

    121212

--- frontend/src/history/js/history.js
+++ frontend/src/history/js/history.js
@@ -89,10 +89,10 @@ function clickFunc() {
   var str = this.id
   var str1 = str.substring(3,5)
   console.log(str1)
-  if(str1 == 'ac'){
+  if(str1 == 'cc'){
     JoinEvent(str); 
   }
-  else if(str1 == 'de'){
+  else if(str1 == 'ec'){
     LeftEvent(str)
   }
 }
